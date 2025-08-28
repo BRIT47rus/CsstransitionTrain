51219d4b0b4d7c15b975ec4b0dcd137b49fb9da4
Vladimir Noutbook <vovirus2@inbox.ru>
2025-08-28 14:43:28 +0300

    created started functions

--- src/components/TrainTwo/Field2.tsx
+++ src/components/TrainTwo/Field2.tsx
@@ -1,28 +1,44 @@
-import { useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import './Fielsd.css';
 import { CSSTransition } from 'react-transition-group';
 
 export const Field2 = () => {
     const [show, setShow] = useState(false);
-    const nodeRef = useRef(null); // без <HTMLElement | null> — для JS/TS
+    const nodeRef = useRef(null);
+    const [state, setState] = useState<string[]>([]);
 
+    const deleteItem = (i: number) => {
+        setState((st) => st.filter((_, it) => it !== i));
+    };
+    let count = 1;
+
+    const addItem = useCallback(() => {
+        count += 1;
+        setState((s) => [...s, 'n' + count]);
+    }, [count]);
     return (
         <div className="field">
-            <button onClick={() => setShow((o) => !o)}>
-                {show ? 'Close' : 'Open'}
-            </button>
-
+            <button onClick={addItem}>{show ? 'Close' : 'Open'}</button>
+            {state &&
+                state.map((item, i) => {
+                    return (
+                        <div
+                            ref={nodeRef}
+                            key={item}
+                            className="fade"
+                            onClick={() => deleteItem(i)}
+                        >
+                            {item}
+                        </div>
+                    );
+                })}
             <CSSTransition
                 nodeRef={nodeRef}
                 in={show}
                 timeout={300}
                 classNames="fade"
                 unmountOnExit
-            >
-                <div ref={nodeRef} className="fade">
-                    Hello world
-                </div>
-            </CSSTransition>
+            ></CSSTransition>
         </div>
     );
 };
